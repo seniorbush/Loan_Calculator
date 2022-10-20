276d52987d3d077492926dd9434869a1fb45e135
Philip Moodie <philipmoodie1989@hotmail.co.uk>
2022-10-20 21:23:49 +0100

    Loan Calculator

---
+++ app.js
@@ -0,0 +1,56 @@
+//
+document.querySelector("#loanForm").addEventListener("submit", (e) => {
+  // INIT FORM VARIABLES
+  let loanAmount = document.getElementById("loan-amount").value;
+  const duration = document.getElementById("duration").value;
+  const apr = document.getElementById("apr").value;
+  //
+  //LOAN CALCULATIONS
+  let percentage = (loanAmount / 100) * parseInt(apr);
+  let totalCost = parseInt(loanAmount) + percentage;
+  let monthlyPayments = totalCost / parseInt(duration);
+  let interest = (percentage / parseInt(duration)).toFixed(2);
+  //
+  document.getElementById("m-payments").innerHTML = `£${monthlyPayments.toFixed(
+    2
+  )}`;
+  document.getElementById("tp").innerHTML = `£${loanAmount}`;
+  document.getElementById("ti").innerHTML = `£${percentage}`;
+  document.getElementById("tc").innerHTML = `£${totalCost}`;
+  //
+  //
+  const tableBody = document.querySelector(".table-body");
+  //
+  tableBody.innerHTML = "";
+  //
+  for (let i = 1; i <= parseInt(duration); i++) {
+    //CREATE NEW TABLE ROW
+    const tableRow = document.createElement("tr");
+    //CREATE ROWS & COLUMNS
+    tableRow.innerHTML = `
+    <td>${i}</td>
+    <td>£${monthlyPayments.toFixed(2)}</td>
+    <td>£${(monthlyPayments - interest).toFixed(2)}</td>
+    <td>£${interest}</td>
+    <td>£${(interest * i).toFixed(2)}</td>
+    <td>£${(totalCost - monthlyPayments * i).toFixed(2)}</td>
+  `;
+
+    tableBody.appendChild(tableRow);
+  }
+
+  e.preventDefault();
+});
+//
+//
+//
+//CLEAR FORM AND TABLE
+document.getElementById("resetForm").addEventListener("click", (e) => {
+  document.getElementById("loanForm").reset();
+  document.querySelector(".table-body").innerHTML = "";
+  document.getElementById("m-payments").innerHTML = "";
+  document.getElementById("tp").innerHTML = "";
+  document.getElementById("ti").innerHTML = "";
+  document.getElementById("tc").innerHTML = "";
+  e.preventDefault();
+});
